d940b9f03cff884c284bad9d24291171dad4991a
Sathish Kumar <sathishkumar.aero51@gmail.com>
2025-07-27 23:00:21 +0200

    add testing using jest

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.jest.json',
+    },
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+};
